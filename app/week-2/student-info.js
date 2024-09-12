409f09803f01b289748488ea7dd468832a6d8141
HuongNguyen0828 <huongnguyen.cada@gmail.com>
2024-09-12 16:31:08 -0600

    Add cprg306-assignments repository
    Follow all steps of instruction

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+export default function StudentInfo() {
+    return (
+        <div>
+             <h1>Thi Huong Nguyen</h1>
+             <Link href="https://github.com/HuongNguyen0828?tab=repositories">My GitHub repository</Link>
+
+        </div>
+       
+    );
+
+}
